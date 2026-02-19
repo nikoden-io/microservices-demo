be7f34f6220d022ddaf2ddf822ba8802a11231ac
Nicolas Denoël <github@nikoden.io>
2026-02-19 21:36:40 +0100

    feat catalog.API : add product + config tools + building blocks MediatR

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(
+    new DependencyContextAssemblyCatalog(assemblies: typeof(Program).Assembly)
+);
+
+builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); })
+    .UseLightweightSessions();
+
+builder.Services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(Program).Assembly); });
+
 var app = builder.Build();
 
+app.MapCarter();
 
 app.Run();
