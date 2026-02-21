4f62a7bb7ffcd57f4963a2518d9d979882a49f06
Nicolas Denoël <github@nikoden.io>
2026-02-21 09:17:13 +0100

    chore catalog.API : configure services extensions and add health config

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,17 +1,23 @@
-// Nuggets
+// .NET
 
+global using System.Reflection;
+
+// Nuggets
 global using Carter;
 global using FluentValidation;
 global using Mapster;
 global using Marten;
 global using Marten.Pagination;
 global using MediatR;
+
 // BuildingBlocks
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
+
 // Catalog.API
 global using Catalog.API.Data;
 global using Catalog.API.Exceptions;
+global using Catalog.API.Extensions;
 global using Catalog.API.Models;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,32 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 
-builder.Services.AddCarter(
-    new DependencyContextAssemblyCatalog(assemblies: assembly)
-);
+builder.Services
+    .AddPresentationServices(assembly)
+    .AddApplicationServices(assembly)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddCrossCuttingServices(builder.Configuration);
 
-builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); })
-    .UseLightweightSessions();
-
-builder.Services.AddMediatR(cfg =>
-{
-    cfg.RegisterServicesFromAssembly(assembly);
-    cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
-    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
-});
-
-builder.Services.AddValidatorsFromAssembly(assembly);
-
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-
-if (builder.Environment.IsDevelopment()) builder.Services.InitializeMartenWith<CatalogInitialData>();
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 var app = builder.Build();
 
 app.MapCarter();
-
 app.UseExceptionHandler();
-
 app.Run();
